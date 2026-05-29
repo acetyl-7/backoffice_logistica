aac2acbe5db293d99f8994ea3e28472debd4db0e
acetyl-7 <ttapmm@gmail.com>
2026-05-29 01:28:05 +0100

    config: update firebase project configuration

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -47,14 +47,15 @@ class DefaultFirebaseOptions {
     projectId: 'logichatinfofirst',
     authDomain: 'logichatinfofirst.firebaseapp.com',
     storageBucket: 'logichatinfofirst.firebasestorage.app',
+    measurementId: 'G-6QJW3P3SXX',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCyofX2gfe3hK5sGkikwvFmXAdd1yq9fSs',
-    appId: '1:909421828884:android:3eec31284cee9aa1e58001',
-    messagingSenderId: '909421828884',
-    projectId: 'cisterpor-fleetsync',
-    storageBucket: 'cisterpor-fleetsync.firebasestorage.app',
+    apiKey: 'AIzaSyCt7Sj7qG3mbe47AR9GtIlFJaJdwqpAfNU',
+    appId: '1:617473071905:android:0bd68fc4c2dd73e90c7c60',
+    messagingSenderId: '617473071905',
+    projectId: 'logichatinfofirst',
+    storageBucket: 'logichatinfofirst.firebasestorage.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
@@ -76,12 +77,13 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyB1o0wTDYUag0F5LgeVDRNhdr0d9t1H4zg',
-    appId: '1:909421828884:web:fc43779bc1daedabe58001',
-    messagingSenderId: '909421828884',
-    projectId: 'cisterpor-fleetsync',
-    authDomain: 'cisterpor-fleetsync.firebaseapp.com',
-    storageBucket: 'cisterpor-fleetsync.firebasestorage.app',
+    apiKey: 'AIzaSyDjn3i7OzS90XKzBRWbOJi59ee64hHOG0M',
+    appId: '1:617473071905:web:bd86f2a5e7cf758c0c7c60',
+    messagingSenderId: '617473071905',
+    projectId: 'logichatinfofirst',
+    authDomain: 'logichatinfofirst.firebaseapp.com',
+    storageBucket: 'logichatinfofirst.firebasestorage.app',
+    measurementId: 'G-MMHRC8MFL9',
   );
 
 }
